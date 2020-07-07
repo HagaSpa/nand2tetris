ed00c61b4a504d17522620c078c2295afacf98b4
HagaSpa <justicesparrow13@gmail.com>
2020-07-07 20:06:12 +0900

    Impl Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,49 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(INIT)
+    @SCREEN
+    D=A      // D <- SCREENのアドレス
+    @addr    // 塗りつぶしたいSCREEN上のワードを表す
+    M=D      // 初期値はSCREENアドレス
+
+    // キーボードの状態を見て、塗りつぶす色を選択
+    @KBD
+    D=M
+    @IFPRESS
+    D;JGT
+    @ELSE
+    0;JMP
+(IFPRESS)
+    @color
+    M=-1     // color=-1 黒 TODO:黒は1では？
+    @ENDIF
+    0;JMP
+(ELSE)
+    @color
+    M=0      // color=0 白
+(ENDIF)
+
+(LOOP)       //塗りつぶすループ
+    @color
+    D=M      // D <- color
+    @addr
+    A=M
+    M=D      // *addr <- color
+    @addr
+    M=M+1
+
+    // 1行は16 * 32 = 512ビットだが、16ビットずつ加算するため1行は1 * 32 = 32回となる。
+    // それが256行あるため、32 * 256 = 8192回が総計算（減算）回数となる。
+    @8192
+    D=A
+    @SCREEN
+    D=D+A
+    @addr
+    D=D-M
+    // 塗り残しがあれば塗りつぶすまで続ける
+    @LOOP
+    D;JGT
+
+    @INIT
+    0;JMP
